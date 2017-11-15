19b82c45906b9f1167e3285d651a15e225d0865d
Nikolai Kotok <lennakz@gmail.com>
2017-11-14 20:43:54 -0800

    1

--- config/db.php
+++ config/db.php
@@ -1,8 +1,10 @@
 <?php
 
+$host = ($_SERVER['REMOTE_ADDR'] == '107.180.24.240') ? 'localhost' : '107.180.24.240';
+
 return [
 	'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=107.180.24.240;dbname=eurohouse_tools',
+	'dsn' => "mysql:host={$host};dbname=eurohouse_tools",
 	'username' => 'eurohouse_tools',
 	'password' => 'mathers',
 	'charset' => 'utf8',
